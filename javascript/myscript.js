514c09e8339f90afa2fd06a99f3fc3e89773834b
gualtierigio <gualtierigio@gmail.com>
2021-08-30 17:17:09 +0200

    inizio ad impostare la parte javascript

---
+++ javascript/myscript.js
@@ -0,0 +1,17 @@
+const burgerName = prompt("inserisci il nome del tuo panino")
+
+if(burgerName == 0){
+    alert("è neccessario indicare un nome")
+}
+
+const baseBurger = 10;
+
+const ingredients = ["Cheese","Egg","Mustard","Tomato","Lettuce","Ketchup"]
+
+
+
+for (let i = 0; i < ingredient.length; i++){
+    if(document.getElementsByClassName(ingredient) = true){
+        
+    }
+}
