39cf2d9e69477dc13fbc0086b2bf38ec662a2621
gualtierigio <gualtierigio@gmail.com>
2021-08-31 10:31:04 +0200

    in fase di correzione

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -1,29 +1,45 @@
-let burgerName = document.getElementById("burgerName");
 
-burgerName.innerHTML = prompt()
 
-const baseBurger = 10;
-
-let ingredientPrice = 5;
-
-let ingredients = document.getElementsByClassName("ingredient");
-
-let burgerPlus = 0;
-
-const calculateBtn = document.getElementById("calculate");
+let calculateBtn = document.getElementById("calculate");
 
 calculateBtn.addEventListener("click", function(){
+
+    if(burgerName.value.length == 0){
+        alert("è neccessario indicare un nome");
+    }else {
+        const baseBurger = 10;
+
+        let ingredientPrice = document.getElementsByClassName("ingredient")
+    }
+
+    let burgerName = document.getElementById("burgerName");
+
+    let ingredients = document.getElementsByClassName("ingredient");
+
+    let coupons = ["pippo", "gino", "pino"]
 
     for (let i = 0; i < ingredients.length; i++){
 
         if(ingredients[i].checked){
-            burgerPlus = baseBurger + ingredientPrice;
+            let costoIngredienti = 5;
+            baseBurger += costoIngredienti;
         
-        }else if(burgerName.length == 0){
-            alert("è neccessario indicare un nome");
         }
+        
+        let coupon = document.getElementById("coupon");
+
+        if (coupon.value.length > 0){
+            if (coupons.includes(coupon.value)){
+                baseBurger = baseBurger * 0.8;
+            }
+        }else {
+            (console.log("non hai lo sconto"))
+        }
+        
     }
 });
 
 
-console.log(burgerPlus);
+console.log(baseBurger;
+
+burgerPlus.innerHTML
